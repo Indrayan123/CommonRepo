ec65b64625e2c91ed3e557a47165212d44aa6021
Indrayan123 <indrayan.chakraborty@gmail.com>
2017-06-21 14:08:22 +0530

    Add files via upload

---
+++ BuildAndCreateSOAArchive.groovy
@@ -0,0 +1,26 @@
+#!groovy
+node(env.label){
+    stage('GIT Source Code Download') {
+         git env.GitURL
+    }       
+    stage('SOA Archive Creation') {
+	script{
+	echo "Running Maven"	 
+    dir(path: env.Subdirectory) {
+    sh ('#!/bin/sh -e\n'+ "/opt/oracle/middleware/oracle_common/modules/org.apache.maven_3.2.5/bin/mvn package")		
+	}
+	}
+	}
+	stage('Stashing Build Folder'){
+	    echo "Current Dir: ${pwd()}"
+	    stash includes: 'Builds/**', name: 'BuildResources'
+	}
+	
+}
+node('master'){
+    stage('UnStashing Build Folder') {
+        dir(path: env.MasterBuildLocation){
+unstash 'BuildResources'
+    }
+}
+}
