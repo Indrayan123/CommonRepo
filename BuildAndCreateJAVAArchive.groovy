bfd36bfea5de36d5aa8a22797dcc1275d314c9c4
Indrayan123 <indrayan.chakraborty@gmail.com>
2017-06-23 16:19:58 +0530

    Add files via upload

---
+++ BuildAndCreateJAVAArchive.groovy
@@ -0,0 +1,33 @@
+node(env.label){
+	
+    stage('GIT Source Code Download') {
+         git env.GitURL
+    }       
+    stage('JAVA WAR Creation') {
+	script{
+	echo "Running Maven"	 
+    dir(path: env.Subdirectory) {
+    sh ('#!/bin/sh -e\n'+ "/opt/oracle/middleware/oracle_common/modules/org.apache.maven_3.2.5/bin/mvn package")		
+	}
+	}
+	}
+	stage('Stashing Build Folder'){
+        dir (path: env.StashFolderName)	{
+	    stash includes: '**', name: 'BuildResources'
+		}
+	}
+	
+}
+node('master'){
+    stage('UnStashing Build Folder') {
+        dir(path: env.MasterBuildLocation){
+		unstash 'BuildResources'
+    }
+}
+}
+node(env.label){
+stage('Clean Directory') {
+		 echo "Cleaning Directory: ${pwd()}"
+         sh ("rm -rf *")
+    }
+}
