c97c3371799e4d9c86a15da7b415d54d1ba42e39
Indrayan123 <indrayan.chakraborty@gmail.com>
2017-06-28 04:42:43 -0500

    Update ScheduledDeploy.groovy

--- ScheduledDeploy.groovy
+++ ScheduledDeploy.groovy
@@ -57,8 +57,15 @@ node(env.label)
 	def deployscript = env.WARDeployscript
 	def buildworkspace = pwd()+'/'+stashbuildfolder
 //	echo soat3url:+"${soat3url}"
+		try{
 	def scriptOp =sh (script: 'java -classpath "/opt/oracle/middleware/wlserver/server/lib/weblogic.jar:/opt/oracle/middleware/wlserver/modules/features/wlst.wls.classpath.jar" weblogic.WLST'+' '+deployscript+' '+soauser+' '+soapass+' '+soat3url+' '+buildworkspace , returnStatus: true)
-		echo "${scriptOp}"	
+		echo "${scriptOp}"
+		}
+		  catch (e)
+    		{
+       		 echo "Inside catch"
+        	 echo "${e}"
+    		}
     }  
 	
 
